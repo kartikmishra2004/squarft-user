21ab31d97e93b9de42ab089e3c2b0ea317465502
Sahni <kumarisahni1204@gmail.com>
2026-03-21 18:48:04 +0530

    auth/index code moved to auth/layout

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,5 +1,17 @@
+// squarft-user/app/(auth)/_layout.jsx
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-    return <Stack screenOptions={{ headerShown: false }} />;
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="onboarding1" />
+            <Stack.Screen name="onboarding2" />
+            <Stack.Screen name="onboarding3" />
+            <Stack.Screen name="onboarding4" />
+            <Stack.Screen name="register" />
+            <Stack.Screen name="login" />
+            <Stack.Screen name="forgot-password" />
+            <Stack.Screen name="otp-verification" />
+        </Stack>
+    );
 }
