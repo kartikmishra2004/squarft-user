24bc3d05752a0cc26189d273bd9b65050de948d8
Kartik Mishra <kartik26nov2004@gmail.com>
2026-03-21 19:01:42 +0530

    Removed animation during transition.

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 
 export default function AuthLayout() {
     return (
-        <Stack screenOptions={{ headerShown: false }}>
+        <Stack screenOptions={{ headerShown: false, animation: "none" }}>
             <Stack.Screen name="onboarding1" />
             <Stack.Screen name="onboarding2" />
             <Stack.Screen name="onboarding3" />
